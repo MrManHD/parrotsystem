151147abb8a404cf7be2322c7879bd089d074729
MrManHD <nikigalaxylp@gmail.com>
2022-06-10 15:15:24 +0200

    add new methods in IParrotGroup

--- parrot-api/src/main/kotlin/net/mrmanhd/parrot/api/group/IParrotGroup.kt
+++ parrot-api/src/main/kotlin/net/mrmanhd/parrot/api/group/IParrotGroup.kt
@@ -1,5 +1,9 @@
 package net.mrmanhd.parrot.api.group
 
+import net.mrmanhd.parrot.api.ParrotApi
+import net.mrmanhd.parrot.api.service.IParrotService
+import net.mrmanhd.parrot.api.service.process.IParrotProgress
+
 /**
  * Created by MrManHD
  * Class create at 10.06.2022 11:36
@@ -8,5 +12,25 @@ package net.mrmanhd.parrot.api.group
 interface IParrotGroup {
 
     fun getName(): String
+
+    fun getPreLoadedWorlds(): List<String>
+
+    fun getMinimumOnlineServiceCount(): Int
+
+    fun getMaxOnlineServiceCount(): Int
+
+    fun isInMaintenance(): Boolean
+
+    fun getAllServices(): List<IParrotService> {
+        return ParrotApi.instance.getServiceHandler().getAllServicesByGroup(this)
+    }
+
+    fun getOnlinePlayersCount(): Int {
+        return getAllServices().sumOf { it.getOnlineCount() }
+    }
+
+    fun createProgress(): IParrotProgress {
+        return ParrotApi.instance.getServiceHandler().createProgress(this)
+    }
 
 }
