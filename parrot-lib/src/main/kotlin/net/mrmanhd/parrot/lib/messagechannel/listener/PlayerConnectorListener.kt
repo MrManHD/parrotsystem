64769159507b61c16112069f7d35fbfffbcb2026
MrManHD <nikigalaxylp@gmail.com>
2022-06-12 21:15:58 +0200

    fix PlayerConnectorListener

--- parrot-lib/src/main/kotlin/net/mrmanhd/parrot/lib/messagechannel/listener/PlayerConnectorListener.kt
+++ parrot-lib/src/main/kotlin/net/mrmanhd/parrot/lib/messagechannel/listener/PlayerConnectorListener.kt
@@ -2,6 +2,10 @@ package net.mrmanhd.parrot.lib.messagechannel.listener
 
 import eu.thesimplecloud.api.message.IMessageListener
 import eu.thesimplecloud.api.network.component.INetworkComponent
+import eu.thesimplecloud.api.player.ICloudPlayer
+import net.mrmanhd.parrot.api.service.IParrotService
+import net.mrmanhd.parrot.lib.api.service.ParrotService
+import net.mrmanhd.parrot.lib.extension.sendChatMessage
 import net.mrmanhd.parrot.lib.messagechannel.dto.PlayerConnectorDTO
 
 /**
@@ -12,7 +16,20 @@ import net.mrmanhd.parrot.lib.messagechannel.dto.PlayerConnectorDTO
 class PlayerConnectorListener : IMessageListener<PlayerConnectorDTO> {
 
     override fun messageReceived(msg: PlayerConnectorDTO, sender: INetworkComponent) {
-        TODO("Not yet implemented")
+        val parrotService = msg.getParrotService()
+        msg.getCloudPlayer().addResultListener { handleCloudPlayerConnection(it, parrotService, msg.playerAlreadyOnSameService) }
+    }
+
+    private fun handleCloudPlayerConnection(
+        cloudPlayer: ICloudPlayer,
+        parrotService: IParrotService?,
+        playerAlreadyOnSameDaemon: Boolean
+    ) {
+        if (parrotService == null) {
+            cloudPlayer.sendToLobby()
+            cloudPlayer.sendChatMessage("connector.failed.service.null", "PCL-25")
+        }
+
     }
 
 }
