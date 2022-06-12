be347ecb1b9ac98aee68a64fa5908bde836eb9cb
MrManHD <nikigalaxylp@gmail.com>
2022-06-12 21:15:00 +0200

    rename connector.failed.service.null message

--- parrot-service/src/main/kotlin/net/mrmanhd/parrot/service/cloud/config/DefaultChatMessageConfig.kt
+++ parrot-service/src/main/kotlin/net/mrmanhd/parrot/service/cloud/config/DefaultChatMessageConfig.kt
@@ -15,7 +15,7 @@ object DefaultChatMessageConfig {
 
     private fun getMessages(): Map<String, String> {
         return hashMapOf(
-            Pair("connector.failed.service.null", "§cEs gab gerade ein Problem beim verbinden des Servers!"),
+            Pair("connector.failed.service.null", "§cEs gab gerade ein Problem beim verbinden des Servers! (#{0})"),
             Pair("connector.failed.round.ends", "§cDiese Runde ist bereits beendet!"),
             Pair("connector.failed.round.full", "§cDiese Runde ist bereits voll!"),
             Pair("connector.failed.round.connected", "§cDu bist bereits in dieser Runde!")
