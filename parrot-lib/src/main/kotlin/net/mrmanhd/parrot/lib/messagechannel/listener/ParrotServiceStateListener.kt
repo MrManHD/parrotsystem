5d81a72a7b8faf73fb4d491c71d1b023d1cd9022
MrManHD <nikigalaxylp@gmail.com>
2022-06-12 18:30:25 +0200

    kick player after stopping parrot service

--- parrot-lib/src/main/kotlin/net/mrmanhd/parrot/lib/messagechannel/listener/ParrotServiceStateListener.kt
+++ parrot-lib/src/main/kotlin/net/mrmanhd/parrot/lib/messagechannel/listener/ParrotServiceStateListener.kt
@@ -2,9 +2,15 @@ package net.mrmanhd.parrot.lib.messagechannel.listener
 
 import eu.thesimplecloud.api.message.IMessageListener
 import eu.thesimplecloud.api.network.component.INetworkComponent
+import eu.thesimplecloud.plugin.extension.syncBukkit
+import net.kyori.adventure.text.Component
+import net.mrmanhd.parrot.api.extension.getBukkitGamePlayers
 import net.mrmanhd.parrot.api.service.IParrotService
 import net.mrmanhd.parrot.lib.api.ParrotLib
+import net.mrmanhd.parrot.lib.extension.sendCloudMessage
 import net.mrmanhd.parrot.lib.messagechannel.dto.ParrotServiceStateDTO
+import net.mrmanhd.parrot.lib.messagechannel.dto.ParrotServiceStateDTO.*
+import org.bukkit.scheduler.BukkitRunnable
 
 /**
  * Created by MrManHD
@@ -15,7 +21,7 @@ class ParrotServiceStateListener : IMessageListener<ParrotServiceStateDTO> {
 
     override fun messageReceived(msg: ParrotServiceStateDTO, sender: INetworkComponent) {
         val parrotService = msg.getParrotService() ?: return
-        if (msg.type == ParrotServiceStateDTO.Type.STARTING) {
+        if (msg.type == Type.STARTING) {
             handleStarting(parrotService)
         } else {
             handleStopping(parrotService)
@@ -30,6 +36,12 @@ class ParrotServiceStateListener : IMessageListener<ParrotServiceStateDTO> {
     private fun handleStopping(parrotService: IParrotService) {
         val localServiceHandler = ParrotLib.instance.localServiceHandler
         localServiceHandler.removeLocalService(parrotService)
+
+        syncBukkit {
+            parrotService.getBukkitGamePlayers().forEach {
+                it.kick(Component.text("§cDas Spiel wird nun neugestartet!"))
+            }
+        }
     }
 
 }
