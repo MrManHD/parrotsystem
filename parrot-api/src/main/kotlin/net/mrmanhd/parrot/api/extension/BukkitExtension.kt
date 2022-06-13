cd28226c09ac9111bf29ac57f4bce9e31d80f52f
MrManHD <nikigalaxylp@gmail.com>
2022-06-13 14:11:52 +0200

    add BukkitExtension in parrot-api

---
+++ parrot-api/src/main/kotlin/net/mrmanhd/parrot/api/extension/BukkitExtension.kt
@@ -0,0 +1,19 @@
+package net.mrmanhd.parrot.api.extension
+
+import net.mrmanhd.parrot.api.ParrotApi
+import net.mrmanhd.parrot.api.service.IParrotService
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+
+/**
+ * Created by MrManHD
+ * Class create at 13.06.2022 14:09
+ */
+ 
+fun Player.getParrotService(): IParrotService? {
+    return ParrotApi.instance.getServiceHandler().getServiceByPlayer(this.uniqueId)
+}
+
+fun IParrotService.getBukkitGamePlayers(): List<Player> {
+    return this.getGamePlayers().mapNotNull { Bukkit.getPlayer(it.getUniqueId()) }
+}
